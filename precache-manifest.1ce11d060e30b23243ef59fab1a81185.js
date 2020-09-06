cbd725fa4ee2f11448f84f92ba9aad8739bd3f35
daechul <44284694+KsPsD@users.noreply.github.com>
2020-09-06 16:30:11 +0900

    Updates

---
+++ precache-manifest.1ce11d060e30b23243ef59fab1a81185.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056def0cadd893c618119fccafc8db51",
+    "url": "/kflix/index.html"
+  },
+  {
+    "revision": "88602edb2727fa6439e7",
+    "url": "/kflix/static/js/2.79bd6790.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kflix/static/js/2.79bd6790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e3f79e9aca50702d64",
+    "url": "/kflix/static/js/main.89be0ace.chunk.js"
+  },
+  {
+    "revision": "149435651e9f8cfbed7d",
+    "url": "/kflix/static/js/runtime-main.f176f891.js"
+  }
+]);
